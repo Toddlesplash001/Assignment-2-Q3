9337a9164af9a6c537ca42413730eae72acf6f73
Toddlesplash001 <118872908+Toddlesplash001@users.noreply.github.com>
2022-12-23 09:06:56 +0530

    Add files via upload

---
+++ Assigment 2 Q3.py
@@ -0,0 +1,7 @@
+a = 56
+b = 10
+print("a & b =",a&b)
+print("a | b =",a|b)
+print("a ^ b =",a^b)
+print("Left shifting both a and b with 2 bits =",a<<2,"=",b<<2)
+print("Right shifting a with 2 bits and b with 4 bits =",a>>2,"and",b>>4)
